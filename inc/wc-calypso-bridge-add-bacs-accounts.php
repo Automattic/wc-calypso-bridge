1c0169f59879de4db5b43399c05b642ba609d70b
Timmy Crawford <timmyc@users.noreply.github.com>
2017-11-17 12:50:28 -0800

    BACS Account Updates (#12)
    * Add ability to update BACS accounts.
    
    * Few more test cases
    
    * Update test description.

--- inc/wc-calypso-bridge-add-bacs-accounts.php
+++ inc/wc-calypso-bridge-add-bacs-accounts.php
@@ -25,3 +25,81 @@ function wc_calypso_bridge_add_bacs_accounts( $response, $gateway, $request ) {
 }
 
 add_filter( 'woocommerce_rest_prepare_payment_gateway', 'wc_calypso_bridge_add_bacs_accounts', 10, 3 );
+
+function wc_calypso_bridge_validate_bacs_accounts( $accounts ) {
+	$valid_keys = array(
+		'account_name',
+		'account_number',
+		'bank_name',
+		'bic',
+		'iban',
+		'sort_code',
+	);
+
+	foreach ( $accounts as $account ) {
+		if ( ! is_array( $account ) ) {
+			return false;
+		}
+
+		$account_keys = array_keys( $account );
+		sort( $account_keys );
+		if ( $account_keys !== $valid_keys ) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function wc_calypso_bridge_format_bacs_accounts( $accounts ) {
+	$formatted_accounts = array();
+
+	foreach ( $accounts as $account ) {
+		$formatted_accounts[] = array(
+			'account_name'    => (string) $account[ 'account_name' ],
+			'account_number'  => (string) $account[ 'account_number' ],
+			'bank_name'       => (string) $account[ 'bank_name' ],
+			'bic'             => (string) $account[ 'bic' ],
+			'iban'            => (string) $account[ 'iban' ],
+			'sort_code'       => (string) $account[ 'sort_code' ],
+		);
+	}
+	return $formatted_accounts;
+}
+
+function wc_calypso_bridge_update_bacs_accounts( $response, $handler, $request ) {
+	if ( is_wp_error( $response ) ) {
+		return $response;
+	}
+
+	if (
+		isset( $handler[ 'callback' ] ) &&
+		is_callable( $handler[ 'callback' ], false, $callable_name )
+	) {
+		switch ( $callable_name ) {
+			case 'WC_REST_Dev_Payment_Gateways_Controller::update_item':
+				$controller = new WC_REST_Payment_Gateways_Controller;
+				$gateway = $controller->get_gateway( $request );
+
+				if (
+					$gateway &&
+					'bacs' === $gateway->id &&
+					isset( $request[ 'settings' ] ) &&
+					isset( $request[ 'settings' ][ 'accounts' ] )
+				) {
+					$new_accounts = (array) $request[ 'settings' ][ 'accounts' ];
+
+					if ( ! wc_calypso_bridge_validate_bacs_accounts( $new_accounts ) ) {
+						return new WP_Error( 'rest_setting_value_invalid', __( 'An invalid setting value was passed.', 'woocommerce' ), array( 'status' => 400 ) );
+					}
+
+					$formatted_accounts = wc_calypso_bridge_format_bacs_accounts( $new_accounts );
+					update_option( 'woocommerce_bacs_accounts', $formatted_accounts );
+				}
+				break;
+		}
+	}
+
+	return $response;
+}
+
+add_filter( 'rest_request_before_callbacks', 'wc_calypso_bridge_update_bacs_accounts', 10, 3 );
--- tests/unit-tests/bacs-accounts.php
+++ tests/unit-tests/bacs-accounts.php
@@ -148,4 +148,192 @@ class BACS_Accounts extends WC_REST_Unit_Test_Case {
 		}
 		return $settings;
 	}
+
+	/**
+	 * Test updating bacs gateway with no account data supplied.
+	 */
+	public function test_update_bacs_payment_gateway_with_no_account_data() {
+		wp_set_current_user( $this->user );
+
+		$request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$request->set_body_params( array(
+			'settings' => array(
+				'title' => 'Pay with bank transfer!',
+			),
+		) );
+		$response = $this->server->dispatch( $request );
+		$bacs = $response->get_data();
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 'Pay with bank transfer!', $bacs['settings']['title']['value'] );
+		$this->assertEquals( array(), $bacs['settings']['accounts']['value'] );
+	}
+
+	/**
+	 * Test updating bacs gateway with account data supplied.
+	 */
+	public function test_update_bacs_payment_gateway_with_account_data() {
+		wp_set_current_user( $this->user );
+
+		$new_accounts = array(
+			array(
+				'account_name'   => 'scrooge mcduck',
+				'account_number' => 'bling',
+				'bank_name'      => 'disney bank',
+				'sort_code'      => '111',
+				'iban'           => '222',
+				'bic'            => 'wat',
+			),
+			array(
+				'sort_code'      => '111',
+				'iban'           => '222',
+				'bic'            => 'wat',
+				'account_name'   => 'something else',
+				'account_number' => '123134844584548',
+				'bank_name'      => 'blaaaaahhhh',
+			)
+		);
+
+		$accounts_request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$accounts_request->set_body_params( array(
+			'settings' => array(
+				'accounts' => $new_accounts,
+			),
+		) );
+		$accounts_response = $this->server->dispatch( $accounts_request );
+		$bacs_accounts = $accounts_response->get_data();
+		$this->assertEquals( 200, $accounts_response->get_status() );
+		$this->assertEquals( $new_accounts, $bacs_accounts['settings']['accounts']['value'] );
+	}
+
+	/**
+	 * Test updating bacs gateway with account, and non-account data supplied.
+	 */
+	public function test_update_bacs_payment_gateway_with_account_data_and_title() {
+		wp_set_current_user( $this->user );
+
+		$new_accounts = array(
+			array(
+				'account_name'   => 'scrooge mcduck',
+				'account_number' => 'bling',
+				'bank_name'      => 'disney bank',
+				'sort_code'      => '111',
+				'iban'           => '222',
+				'bic'            => 'wat',
+			)
+		);
+
+		$accounts_request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$accounts_request->set_body_params( array(
+			'settings' => array(
+				'accounts' => $new_accounts,
+				'title'    => 'Give me money',
+			),
+		) );
+		$accounts_response = $this->server->dispatch( $accounts_request );
+		$bacs_accounts = $accounts_response->get_data();
+		$this->assertEquals( 200, $accounts_response->get_status() );
+		$this->assertEquals( $new_accounts, $bacs_accounts['settings']['accounts']['value'] );
+		$this->assertEquals( 'Give me money', $bacs_accounts['settings']['title']['value'] );
+	}
+
+	/**
+	 * Test updating bacs gateway with account invalid data supplied.
+	 */
+	public function test_update_bacs_payment_gateway_with_malformed_account_data() {
+		wp_set_current_user( $this->user );
+
+		$new_accounts = array(
+			array(
+				'bad' => 'data',
+				'omg' => 'jibberish',
+			)
+		);
+
+		$request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$request->set_body_params( array(
+			'settings' => array(
+				'accounts' => $new_accounts,
+			),
+		) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+	}
+
+	/**
+	 * Test updating bacs gateway with empty accounts.
+	 */
+	public function test_update_bacs_payment_gateway_with_empty_account_array() {
+		wp_set_current_user( $this->user );
+
+		$account_data = array(
+			array(
+				'account_name'   => 'chicken and ribs',
+				'account_number' => '123yummy',
+				'bank_name'      => 'tasty bbq',
+				'sort_code'      => 'half rack',
+				'iban'           => 'brisket',
+				'bic'            => 'yes',
+			)
+		);
+		update_option( 'woocommerce_bacs_accounts', $account_data );
+
+		$response = $this->server->dispatch( new WP_REST_Request( 'GET', '/wc/v3/payment_gateways' ) );
+		$gateways   = $response->get_data();
+		$this->assertEquals( 200, $response->get_status() );
+		$filtered_gateways = wp_filter_object_list( $gateways, array( 'id' => 'bacs' ) );
+		$bacs = array_pop( $filtered_gateways );
+		$this->assertTrue( array_key_exists( 'accounts', $bacs[ 'settings' ] ) );
+		$this->assertEquals( $bacs[ 'settings' ][ 'accounts' ], array(
+			'id'    => 'accounts',
+			'value' => $account_data,
+		) );
+
+		$empty_accounts = array( 
+			array(
+				'account_name'   => '',
+				'account_number' => '',
+				'bank_name'      => '',
+				'sort_code'      => '',
+				'iban'           => '',
+				'bic'            => '',
+			)
+		);
+		$request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$request->set_body_params( array(
+			'settings' => array(
+				'accounts' => $empty_accounts,
+			),
+		) );
+		$response = $this->server->dispatch( $request );
+		$bacs   = $response->get_data();
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertTrue( array_key_exists( 'accounts', $bacs[ 'settings' ] ) );
+		$this->assertEquals( $bacs[ 'settings' ][ 'accounts' ], array(
+			'id' => 'accounts',
+			'value' => $empty_accounts,
+		) );
+	}
+
+	/**
+	 * Test updating bacs gateway with no account data, to effectively "delete"
+	 * BACS accounts
+	 */
+	public function test_update_bacs_payment_gateway_with_empty_account_data_array() {
+		wp_set_current_user( $this->user );
+
+		$request = new WP_REST_Request( 'POST', '/wc/v3/payment_gateways/bacs' );
+		$request->set_body_params( array(
+			'settings' => array(
+				'accounts' => array(),
+			),
+		) );
+		$response = $this->server->dispatch( $request );
+		$bacs   = $response->get_data();
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertTrue( array_key_exists( 'accounts', $bacs[ 'settings' ] ) );
+		$this->assertEquals( $bacs[ 'settings' ][ 'accounts' ], array(
+			'id' => 'accounts',
+			'value' => array(),
+		) );
+	}
 }
