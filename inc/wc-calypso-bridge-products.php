b8cb20169737cf14a1c9bf8f75c7aa70249eee8c
Allen Snook <allen@allendav.com>
2018-02-16 12:24:46 -0800

    Add created_via to products and jetpack sync for products

---
+++ inc/wc-calypso-bridge-products.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Adds additional meta to products for the Store on .com experience.
+ * See also wc-calypso-bridge-jetpack-sync.php
+ *
+ * @since 0.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function wc_calypso_bridge_products_get_context() {
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		$referrer = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : '';
+		$known_referrers = array(
+			'page=product_importer' => 'ajax-product-importer',
+			'/post-new.php'         => 'ajax-post-new',
+		);
+
+		foreach ( $known_referrers as $known_referrer => $context ) {
+			if ( false !== strpos( $referrer, $known_referrer ) ) {
+				return $context;
+			}
+		}
+
+		return 'ajax-unknown';
+	}
+
+	$request_uri = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '';
+	$triggers = array(
+		'/wp-json/wc/v2'          => 'rest-api-v2',
+		'/?rest_route=%2Fwc%2Fv2' => 'rest-api-v2-rest-route',
+		'/wp-json/wc/v3'          => 'rest-api-v3',
+		'/?rest_route=%2Fwc%2Fv3' => 'rest-api-v3-rest-route',
+		'/post-new.php'           => 'post-new',
+	);
+
+	foreach ( $triggers as $fragment => $context ) {
+		if ( false !== strpos( $_SERVER[ 'REQUEST_URI' ], $fragment ) ) {
+			return $context;
+		}
+	}
+
+	return 'unknown';
+}
+
+function wc_calypso_bridge_products_wp_insert_post( $post_ID, $post, $update ) {
+	if ( $update ) {
+		return;
+	}
+
+	$product_post_types = array( 'product', 'product_variation' );
+	if ( ! in_array( $post->post_type, $product_post_types ) ) {
+		return;
+	}
+
+	$created_via = wc_calypso_bridge_products_get_context();
+	update_post_meta( $post_ID, '_created_via', $created_via );
+}
+
+add_action( 'wp_insert_post', 'wc_calypso_bridge_products_wp_insert_post', 10, 4 );
