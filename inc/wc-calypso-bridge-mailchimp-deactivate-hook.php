489c5ac1402d60f111c778aca16e7d00aca3009a
Timmy Crawford <timmydcrawford@gmail.com>
2018-04-06 14:20:30 -0700

    Updates per feedback.

--- inc/wc-calypso-bridge-mailchimp-deactivate-hook.php
+++ inc/wc-calypso-bridge-mailchimp-deactivate-hook.php
@@ -11,7 +11,11 @@ register_deactivation_hook( 'mailchimp-for-woocommerce/mailchimp-woocommerce.php
 
 function wc_calypso_bridge_clear_mailchimp_tables() {
 	global $wpdb;
-	$log = new WC_Logger();
+	$logger = false;
+
+	if ( class_exists( 'WC_Logger' ) ) {
+		$logger = new WC_Logger();
+	}
 
 	$mailchimp_tables = array(
 		"{$wpdb->prefix}queue",
@@ -19,13 +23,17 @@ function wc_calypso_bridge_clear_mailchimp_tables() {
 		"{$wpdb->prefix}mailchimp_carts",
 	);
 
-	foreach( $mailchimp_tables as $table ) {
-		$sql = "TRUNCATE TABLE `$table`";
+	foreach( (array) $mailchimp_tables as $table ) {
+		$sql = $wpdb->prepare( "TRUNCATE TABLE `$table`" );
 
 		if ( $wpdb->query( $sql ) ) {
-			$log->add( 'mailchimp-for-woocommerce', "Plugin Deactivated: success clearing `$table` table." );
+			$log_message = "Plugin Deactivated: success clearing `$table` table.";
 		} else {
-			$log->add( 'mailchimp-for-woocommerce', "Plugin Deactivated: FAILURE clearing `$table` table." );
+			$log_message = "Plugin Deactivated: FAILURE clearing `$table` table.";
+		}
+
+		if ( $logger ) {
+			$logger->add( 'mailchimp-for-woocommerce', $log_message );
 		}
 	}
 }
