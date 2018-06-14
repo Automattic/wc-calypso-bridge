2e3aacfcf23616c6aa34653582e044eb3dce80fe
Timmy Crawford <timmyc@users.noreply.github.com>
2018-06-14 06:28:41 -0700

    Prep for release of v0.2.3 (#54)

--- wc-calypso-bridge-class.php
+++ wc-calypso-bridge-class.php
@@ -7,7 +7,7 @@ class WC_Calypso_Bridge {
 	/**
 	 * Current version of the plugin.
 	 */
-	const CURRENT_VERSION = '0.2.2';
+	const CURRENT_VERSION = '0.2.3';
 
 	/**
 	 * Minimum woocommerce version needed to run this plugin.
