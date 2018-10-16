67b0446176637efc62fdc8be8c507bdae12de652
Justin Shreve <justin.shreve@gmail.com>
2018-10-16 11:55:21 -0400

    Move legacy store on wp.com code (#56)
    * Refactor legacy store on wp.com code to a different folder, so we can conditionally load it, and make room for the new compatibility layer for hosted stores.
    
    * Fix up phpunit tests on certain versions of WC/wc-api-dev, and remove extra error_log.

--- wc-calypso-bridge-class.php
+++ wc-calypso-bridge-class.php
@@ -3,17 +3,6 @@
  * WC Calypso Bridge
  */
 class WC_Calypso_Bridge {
-
-	/**
-	 * Current version of the plugin.
-	 */
-	const CURRENT_VERSION = '0.2.3';
-
-	/**
-	 * Minimum woocommerce version needed to run this plugin.
-	 */
-	const WC_MIN_VERSION = '3.0.0';
-
 	/**
 	 * Paths to assets act oddly in production
 	 */
@@ -29,93 +18,8 @@ class WC_Calypso_Bridge {
 	 * Constructor.
 	 */
 	public function __construct() {
-		add_action( 'woocommerce_init', array( $this, 'init' ), 20 );
-	}
-
-	/**
-	 * Loads API includes and registers routes.
-	 */
-	function init() {
-		if ( $this->is_woocommerce_valid() ) {
-			$this->includes();
-			// Ensure wc-api-dev has already registered routes
-			add_action( 'rest_api_init', array( $this, 'register_routes' ), 20 );
-		}
-	}
-
-	/**
-	 * Makes sure WooCommerce is installed and up to date.
-	 */
-	public function is_woocommerce_valid() {
-		return (
-			class_exists( 'woocommerce' ) &&
-			version_compare(
-				get_option( 'woocommerce_db_version' ),
-				WC_Calypso_Bridge::WC_MIN_VERSION,
-				'>='
-			)
-		);
-	}
-
-	/**
-	 * Includes.
-	 */
-	public function includes() {
-		/** Patches includes */
-		include_once( dirname( __FILE__ ) . '/inc/class-customizer-guided-tour.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-add-bacs-accounts.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-allowed-redirect-hosts.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-cheque-defaults.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-disable-publicize.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-email-order-url.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-enable-auto-update-db.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-hide-alerts.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-jetpack-hotfixes.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-jetpack-sync.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-mailchimp-deactivate-hook.php' );		
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-mailchimp-no-redirect.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-masterbar-menu.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-paypal-defaults.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-paypal-method-supports.php' );
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-products.php' );
-
-		/** API includes */
-		include_once( dirname( __FILE__ ) . '/api/class-wc-calypso-bridge-send-invoice-controller.php' );
-		include_once( dirname( __FILE__ ) . '/api/class-wc-calypso-bridge-settings-email-groups-controller.php' );
-		include_once( dirname( __FILE__ ) . '/api/class-wc-calypso-bridge-data-counts-controller.php' );
-		include_once( dirname( __FILE__ ) . '/api/class-wc-calypso-bridge-product-reviews-controller.php' );
-
-		if ( class_exists( 'MailChimp_Woocommerce' ) ) {
-			include_once( dirname( __FILE__ ) . '/api/class-wc-calypso-bridge-mailchimp-settings-controller.php' );
-		}
-
-	}
-
-	/**
-	 * Register REST API routes.
-	 *
-	 * New endpoints/controllers can be added here.
-	 */
-	public function register_routes() {
-		$controllers = array(
-			'WC_Calypso_Bridge_Send_Invoice_Controller',
-			'WC_Calypso_Bridge_Settings_Email_Groups_Controller',
-			'WC_Calypso_Bridge_Data_Counts_Controller',
-			'WC_Calypso_Bridge_Product_Reviews_Controller',
-		);
-
-		if ( class_exists( 'MailChimp_Woocommerce' ) ) {
-				$controllers[] = 'WC_Calypso_Bridge_MailChimp_Settings_Controller';
-		}
-
-		foreach ( $controllers as $controller ) {
-			$controller_instance = new $controller();
-			$controller_instance->register_routes();
-		}
-
-		// We include it here because rest_api_init is a proper context for mocked add_settings_error function
-		include_once( dirname( __FILE__ ) . '/inc/wc-calypso-bridge-mailchimp-add-settings-error.php' );
-	}
+		//add_action( 'woocommerce_init', array( $this, 'init' ), 20 );
+    }
 
 	/**
 	 * Class instance.
