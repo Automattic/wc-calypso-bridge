0bcb80f62088657d2fd1b08cd7b394ba0c58f83d
Justin Shreve <justin.shreve@gmail.com>
2018-10-19 13:23:08 -0400

    Add missing tax pages for core, and support for 25 extensions

---
+++ includes/connect/woocommerce-zapier.php
@@ -0,0 +1,18 @@
+<?php
+wc_calypso_bridge_connect_page( array(
+	'screen_id' => 'edit-wc_zapier_feed',
+	'menu'      => 'woocommerce',
+	'submenu'   => 'edit.php?post_type=wc_zapier_feed',
+) );
+
+wc_calypso_bridge_connect_page( array(
+	'screen_id' => 'add-wc_zapier_feed',
+	'menu'      => 'woocommerce',
+	'submenu'   => 'edit.php?post_type=wc_zapier_feed',
+) );
+
+wc_calypso_bridge_connect_page( array(
+	'screen_id' => 'wc_zapier_feed',
+	'menu'      => 'woocommerce',
+	'submenu'   => 'edit.php?post_type=wc_zapier_feed',
+) );
