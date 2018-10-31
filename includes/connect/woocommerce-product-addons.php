91053ea93771d79d7dc3450eb49ac4f0ada1bdf8
Justin Shreve <justin.shreve@gmail.com>
2018-10-31 16:19:38 -0400

    Fix masterbar hover state and product add-ons compatibility (#125)

--- includes/connect/woocommerce-product-addons.php
+++ includes/connect/woocommerce-product-addons.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Register Product Add-on Pages
+ *
+ * @package WC_Calypso_Bridge/Classes
+ */
+
 wc_calypso_bridge_connect_page(
 	array(
 		'screen_id' => 'product_page_global_addons',
+		'menu'      => 'edit.php?post_type=product',
+	)
+);
+
+wc_calypso_bridge_connect_page(
+	array(
+		'screen_id' => 'product_page_addons',
 		'menu'      => 'edit.php?post_type=product',
 	)
 );
