f7c4429f06e76f037a366ffe16c8e32a2d62ec6f
Joshua Flowers <joshuatf@gmail.com>
2018-11-01 08:54:49 -0500

    Replace dismissal notice buttons

--- assets/js/calypsoify.js
+++ assets/js/calypsoify.js
@@ -64,4 +64,11 @@
         $( this ).prepend( '<span class="wc-calypso-bridge-notice-icon-wrapper">' + icon + '</span>' );
     } );
 
+    /**
+     * Replace dismissal buttons
+     */
+    $( document ).ready( function() {
+        $( '.notice-dismiss' ).html( '<svg class="gridicon gridicons-cross" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M18.36 19.78L12 13.41l-6.36 6.37-1.42-1.42L10.59 12 4.22 5.64l1.42-1.42L12 10.59l6.36-6.36 1.41 1.41L13.41 12l6.36 6.36z"/></g></svg>' );
+    } );
+
 } )( jQuery );
