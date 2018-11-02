7ee7a48d8f6815083310bc65df0e4632b13489c1
Joshua T Flowers <joshuatf@gmail.com>
2018-11-02 16:29:24 -0500

    Track events inside OBW and checklist (#133)
    * Add OBW step completion tracking
    
    * Add task list click tracking
    
    * Add checklist completion tracking
    
    * Add tracking for address edit in the OBW
    
    * Track when calypsoify is turned on or off
    
    * Rename and refactor calypsoify toggle event
    
    * Add blog ID to all jetpack_tracks_record_event
    
    * Update event names
    
    * Pass task ID to event props
    
    * Create record_event and check if jetpack_tracks_record_event exists
    
    * Check if window.jpTracksAJAX is defined before recording event
    
    * Change task IDs from ints to strings

---
+++ assets/js/calypsoify.js
@@ -0,0 +1,46 @@
+( function( $ ) {
+    'use strict';
+
+    /**
+     * Record checklist task click
+     */
+    $( '.checklist__task-title a, .checklist__task-secondary a' ).click( function() {
+        var $task = $( this ).closest( '.checklist__task' )
+        var status = $task.hasClass( 'is-completed' ) ? 'complete' : 'incomplete';
+        var taskId = $task.data('id');
+        var taskTitle = $task.data('title');
+
+        window.jpTracksAJAX.record_ajax_event(
+            'atomic_wc_tasklist_click',
+            'click',
+            {
+                id: taskId,
+                title: taskTitle, 
+                status: status,
+            }
+        );
+    } );
+
+    /**
+     * Track 'I'm done' completion on task list
+     */
+    $( '.setup-footer a' ).click( function(e) {
+        e.preventDefault();
+        var progressNumber = $( '.checklist__header-progress-number' ).text().split( '/' );
+        var complete = progressNumber[0];
+        var total = progressNumber[1];
+        var percentage = parseFloat( complete / total ).toFixed( 2 ) * 100;
+        if (window.jpTracksAJAX) {
+            window.jpTracksAJAX.record_ajax_event(
+                'atomic_wc_tasklist_finish',
+                'click',
+                { 
+                    complete: complete,
+                    total: total,
+                    percentage: percentage
+                }
+            );
+        }
+    } );
+
+} )( jQuery );
