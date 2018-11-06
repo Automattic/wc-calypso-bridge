5d14e7fd76ec77e6e2fa8a39dab568ee6b9435d8
Joshua T Flowers <joshuatf@gmail.com>
2018-11-06 16:12:24 -0500

    Add mobile styling to checklist page (#140)
    * Add mobile styling to checklist page
    
    * Fix tab indentation

--- includes/class-wc-calypso-bridge-admin-setup-checklist.php
+++ includes/class-wc-calypso-bridge-admin-setup-checklist.php
@@ -565,12 +565,10 @@ class WC_Calypso_Bridge_Admin_Setup_Checklist {
 					?>
 				</small>
 			</div>
-			<?php if ( true === $task['condition'] ) { ?>
-				<div class="checklist__task-icon">
-					<svg class="gridicon gridicons-checkmark" height="18" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M9 19.414l-6.707-6.707 1.414-1.414L9 16.586 20.293 5.293l1.414 1.414"></path></g></svg>
-				</div>
-			<?php } ?>
+			<div class="checklist__task-icon">
+				<svg class="gridicon gridicons-checkmark" height="18" width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M9 19.414l-6.707-6.707 1.414-1.414L9 16.586 20.293 5.293l1.414 1.414"></path></g></svg>
 			</div>
+		</div>
 		<?php
 	}
 }
