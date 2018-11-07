8e6e360036729a7077999b63f954e9dba145950c
Timmy Crawford <timmydcrawford@gmail.com>
2018-11-07 04:53:24 -0800

    WPCS

--- includes/class-wc-calypso-bridge-hide-alerts.php
+++ includes/class-wc-calypso-bridge-hide-alerts.php
@@ -14,38 +14,38 @@ defined( 'ABSPATH' ) || exit;
  */
 class WC_Calypso_Bridge_Hide_Alerts {
 
-    /**
-     * Class instance.
-     *
-     * @var WC_Calypso_Bridge_Hide_Alerts instance
-     */
-    protected static $instance = false;
+	/**
+	 * Class instance.
+	 *
+	 * @var WC_Calypso_Bridge_Hide_Alerts instance
+	 */
+	protected static $instance = false;
 
-    /**
-     * Get class instance
-     */
-    public static function get_instance() {
-        if ( ! self::$instance ) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
+	/**
+	 * Get class instance
+	 */
+	public static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Constructor
-     */
-    private function __construct() {
-        add_action( 'admin_init', array( $this, 'hide_woo_obw_alert' ) );
-    }
-    
-    /**
-     * Prevents the OBW admin alert from Woo Core from being shown
-     */
-    function hide_woo_obw_alert() {
-        if ( class_exists( 'WC_Admin_Notices' ) ) {
-            WC_Admin_Notices::remove_notice( 'install' );
-        }
-    }
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+		add_action( 'admin_init', array( $this, 'hide_woo_obw_alert' ) );
+	}
+
+	/**
+	 * Prevents the OBW admin alert from Woo Core from being shown
+	 */
+	function hide_woo_obw_alert() {
+		if ( class_exists( 'WC_Admin_Notices' ) ) {
+			WC_Admin_Notices::remove_notice( 'install' );
+		}
+	}
 
 }
 $wc_calypso_bridge_hide_alerts = WC_Calypso_Bridge_Hide_Alerts::get_instance();
