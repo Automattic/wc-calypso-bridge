44721ec863b078ed2c78ab3cee194691e352b658
Jeffrey Pearce <jeffikus@gmail.com>
2018-11-11 23:45:50 +0200

    Adds default Customizer values overriding Storefront and Powerpack to provide a more streamlined experience.

--- includes/class-wc-calypso-bridge-themes-setup.php
+++ includes/class-wc-calypso-bridge-themes-setup.php
@@ -39,7 +39,10 @@ class WC_Calypso_Bridge_Themes_Setup {
 
         // Suppress WC Admin Notices
 		add_action( 'admin_head', array( $this, 'suppress_admin_notices' ) );
-		add_filter( 'woocommerce_helper_suppress_connect_notice', '__return_true' );
+        add_filter( 'woocommerce_helper_suppress_connect_notice', '__return_true' );
+        // Set default theme values
+        add_action( 'init', array( $this, 'set_theme_default_values' ) ); 
+        
 	}
 
     /**
@@ -90,7 +93,34 @@ class WC_Calypso_Bridge_Themes_Setup {
 		$deleted = delete_option( 'wpa_activation_notice' );
 		// Suppress all other WC Admin Notices not specified above
 		WC_Admin_Notices::remove_all_notices();
-	}
+    }
+    
+    public function check_if_defaults_already_setup() {
+        $wpcom_ec_plan_theme_defaults = get_option( 'wpcom_ec_plan_theme_defaults', false );
+        return $wpcom_ec_plan_theme_defaults;
+    }
+
+    public function set_theme_default_values() {
+        // Should either be on theme activation or set to have been saved
+        if ( ! $this->check_if_defaults_already_setup() ) {
+            set_theme_mod( 'sp_product_layout', 'full-width' ); // enables Full width single product page.
+            set_theme_mod( 'sp_shop_layout', 'full-width' ); // enables Full width shop archive pages.
+            set_theme_mod( 'sph_hero_enable', 'enable' ); // enables Parallax Hero on homepage.
+            set_theme_mod( 'sph_hero_heading_text', esc_attr__( 'Welcome Test', 'storefront' ) ); // Heading Text for Parallax Hero.
+            set_theme_mod( 'sph_hero_text', sprintf( esc_attr__( 'This is your homepage which is what most visitors will see when they first visit your shop.%sYou can change this text by editing the "Parallax Hero" Section via the "Powerpack" settings in the Customizer on the left hand side of your screen.', 'storefront' ), PHP_EOL . PHP_EOL ) ); // Content for Parallax Hero.
+            if ( 0 < wc_get_page_id( 'shop' ) ) {
+                set_theme_mod( 'sph_hero_button_url', get_permalink( wc_get_page_id( 'shop' ) ) ); // Set button url to the shop page instead of home if the shop page exists.
+            }
+            set_theme_mod( 'sp_homepage_content', false ); // Removes default Welcome banner from starter content.
+            set_theme_mod( 'sp_homepage_top_rated', false ); // Removes Top Rated Products area from starter content.
+            set_theme_mod( 'sp_homepage_on_sale', false ); // Removes On Sale Products area from starter content.
+            set_theme_mod( 'sp_homepage_best_sellers', false ); // Removes Best Sellers Products area from starter content.
+            update_option( 'woocommerce_demo_store', 'yes' ); // enables demo store notice.
+
+            // Save option that says the setup has been run already.
+            update_option( 'wpcom_ec_plan_theme_defaults', true );
+        }
+    }    
 
 }
 
