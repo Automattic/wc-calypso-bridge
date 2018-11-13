41b2ca53650be8a06a848a951bc9f1f87ff5bc0f
Joshua Flowers <joshuatf@gmail.com>
2018-11-13 14:04:40 +0800

    Add store action header bar

--- class-wc-calypso-bridge.php
+++ class-wc-calypso-bridge.php
@@ -88,6 +88,7 @@ class WC_Calypso_Bridge {
 		include_once dirname( __FILE__ ) . '/includes/class-wc-calypso-bridge-hide-alerts.php';
 		include_once dirname( __FILE__ ) . '/includes/class-wc-calypso-bridge-pagination.php';
 		include_once dirname( __FILE__ ) . '/includes/class-wc-calypso-bridge-taxonomies.php';
+		include_once dirname( __FILE__ ) . '/includes/class-wc-calypso-bridge-action-header.php';
 		include_once dirname( __FILE__ ) . '/includes/gutenberg.php';
 		include_once dirname( __FILE__ ) . '/vendor/automattic/gridicons/php/gridicons.php';
 
--- includes/class-wc-calypso-bridge-menus.php
+++ includes/class-wc-calypso-bridge-menus.php
@@ -48,30 +48,12 @@ class WC_Calypso_Bridge_Menus {
 		// We want the menu handler hooks to run late, so that other plugins hooking in here can make changes first.
 		$late_priority = 1000;
 		if ( is_wc_calypso_bridge_page() ) {
-			add_action( 'in_admin_header', array( $this, 'insert_sidebar_html' ) );
 			remove_action( 'in_admin_header', array( Jetpack_Calypsoify::getInstance(), 'insert_sidebar_html' ) );
 
 			add_action( 'admin_head', array( $this, 'woocommerce_menu_handler' ) );
 		} else {
 			add_action( 'admin_head', array( $this, 'calypsoify_menu_handler' ) );
 		}
-	}
-
-	/**
-	 * Creates a top level "WooCommerce" Calypso item, so that users can easily navigate back to the real Calypso.
-	 *
-	 * TODO: Final naming on this (Store vs eCommerce vs WooCommerce)
-	 * TODO: id="calypso-woocommerce" instead, once we start loading some CSS.
-	 */
-	public function insert_sidebar_html() { ?>
-		<a href="<?php echo esc_url( 'https://wordpress.com/stats/day/' . Jetpack::build_raw_urls( home_url() ) ); ?>" id="calypso-sidebar-header">
-			<?php echo get_gridicon( 'gridicons-chevron-left' ); // WPCS: XSS ok. ?>
-			<ul>
-				<li id="calypso-sitename"><?php bloginfo( 'name' ); ?></li>
-				<li id="calypso-plugins"><?php esc_html_e( 'WooCommerce', 'wc-calypso-bridge' ); ?></li>
-			</ul>
-		</a>
-		<?php
 	}
 
 	/**
