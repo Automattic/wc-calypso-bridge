24f061aa97849c8567e06bacc6102f478f5c0a73
Timmy Crawford <timmydcrawford@gmail.com>
2020-04-29 15:40:23 -0700

    Register TaxJar settings page.

---
+++ includes/connect/taxjar-simplified-taxes-for-woocommerce.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Register the TaxJar Settings Page.
+ *
+ * @package WC_Calypso_Bridge
+ * @since 1.1.5
+ * @version 1.0.0
+ */
+
+wc_calypso_bridge_connect_page(
+	array(
+		'screen_id' => 'woocommerce_page_wc-settings-taxjarz-integration',
+		'menu'      => 'woocommerce',
+	)
+);
