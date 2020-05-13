3df3edbfd09443188a7d1192f595641f1e8c33f4
Timmy Crawford <timmydcrawford@gmail.com>
2020-05-13 10:16:04 -0700

    Allow new Site Profiler OBW to run in ecomm plan.

--- class-wc-calypso-bridge.php
+++ class-wc-calypso-bridge.php
@@ -195,7 +195,7 @@ class WC_Calypso_Bridge {
 	 * Updates required UI elements for calypso bridge pages only.
 	 */
 	public function load_ui_elements() {
-		if ( is_wc_calypso_bridge_page() || ( isset( $_GET['page'] ) && 'wc-setup' === $_GET['page'] ) ) {
+		if ( is_wc_calypso_bridge_page() ) {
 			add_action( 'admin_print_styles', array( $this, 'enqueue_calypsoify_scripts' ), 11 );
 
 			include_once dirname( __FILE__ ) . '/includes/class-wc-calypso-bridge-breadcrumbs.php';
