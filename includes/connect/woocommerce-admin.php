244007d0333be735cc27745ecebaf004e70ad856
Sam Seay <samueljseay@gmail.com>
2020-07-09 11:00:24 +1200

    Correct the menu path for analytics so that it shows in the main menu. (#570)
    * Correct the menu path for analytics so that it shows in the main menu.
    * Keep the legacy page connect to support when homescreen is inactive.

--- includes/connect/woocommerce-admin.php
+++ includes/connect/woocommerce-admin.php
@@ -18,6 +18,14 @@ wc_calypso_bridge_connect_page(
 wc_calypso_bridge_connect_page(
 	array(
 		'screen_id' => 'woocommerce-revenue',
+		'menu'      => 'wc-admin&path=/analytics/overview',
+		'submenu'   => 'wc-admin&path=/analytics/overview',
+	)
+);
+
+wc_calypso_bridge_connect_page(
+	array(
+		'screen_id' => 'woocommerce-revenue',
 		'menu'      => 'wc-admin&path=/analytics/revenue',
 		'submenu'   => 'wc-admin&path=/analytics/revenue',
 	)
