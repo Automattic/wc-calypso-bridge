a9d6a643fe4c7764279c6569070dc484d88b6f19
Fernando Marichal <contacto@fernandomarichal.com>
2020-09-14 10:00:47 -0300

    Added unit tests
    This commit adds unit tests

--- tests/unit-tests/test-class-wc-calypso-bridge-setup.php
+++ tests/unit-tests/test-class-wc-calypso-bridge-setup.php
@@ -28,4 +28,32 @@ class WC_Calypso_Bridge_Setup_Test extends WC_Calypso_Bridge_Test {
 		$this->assertEquals( $filtered_products['physical']['label'], 'Physical products' );
 	}
 
+	/**
+	 * Test removing CBD industry from industries list.
+	 *
+	 * @since 3.5.0
+	 */
+	public function test_remove_not_allowed_industries() {
+		$wc_calypso_bridge_setup = WC_Calypso_Bridge_Setup::get_instance();
+		$industries              = array(
+			'fashion-apparel-accessories'     => array(
+				'label'             => 'Fashion, apparel, and accessories',
+				'use_description'   => false,
+				'description_label' => '',
+			),
+			'cbd-other-hemp-derived-products' => array(
+				'label'             => 'CBD and other hemp-derived products',
+				'use_description'   => false,
+				'description_label' => '',
+			),
+			'other'                           => array(
+				'label'             => 'Other',
+				'use_description'   => true,
+				'description_label' => 'Description',
+			),
+		);
+		$filtered_industries     = $wc_calypso_bridge_setup->remove_not_allowed_industries( $industries );
+		$this->assertEquals( count( $filtered_industries ), 2 );
+	}
+
 }
