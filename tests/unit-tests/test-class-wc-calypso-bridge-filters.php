bc83c2d0b05bdac208864ea8df85ce675566f848
Fernando Marichal <contacto@fernandomarichal.com>
2020-10-14 11:55:13 -0300

    Fixed tests
    This commit splits the "test_remove_not_allowed_industries" test in two.

--- tests/unit-tests/test-class-wc-calypso-bridge-filters.php
+++ tests/unit-tests/test-class-wc-calypso-bridge-filters.php
@@ -8,9 +8,9 @@ class WC_Calypso_Bridge_Filters_Test extends WC_Calypso_Bridge_Test {
 	 * Test removing `CBD and other hemp-derived products` option from industries list
 	 *
 	 */
-	public function test_remove_not_allowed_industries() {
-		$wc_calypso_bridge_filters     = WC_Calypso_Bridge_Filters::get_instance();
-		$industries                    = array(
+	public function test_remove_not_allowed_industries_from_industries_list() {
+		$wc_calypso_bridge_filters = WC_Calypso_Bridge_Filters::get_instance();
+		$industries                = array(
 			'fashion-apparel-accessories'     => array(
 				'label'             => 'Fashion, apparel, and accessories',
 				'use_description'   => false,
@@ -27,14 +27,22 @@ class WC_Calypso_Bridge_Filters_Test extends WC_Calypso_Bridge_Test {
 				'description_label' => 'Description',
 			),
 		);
-		$industries_variation          = array(
+		$filtered_industries       = $wc_calypso_bridge_filters->remove_not_allowed_industries( $industries );
+		$this->assertEquals( count( $filtered_industries ), 2 );
+	}
+
+	/**
+	 * Test removing `CBD and other hemp-derived products` option from slugs list
+	 *
+	 */
+	public function test_remove_not_allowed_industries_from_slug_list() {
+		$wc_calypso_bridge_filters = WC_Calypso_Bridge_Filters::get_instance();
+		$industries_slugs          = array(
 			array( 'slug' => 'fashion-apparel-accessories' ),
 			array( 'slug' => 'cbd-other-hemp-derived-products' ),
 			array( 'slug' => 'other' ),
 		);
-		$filtered_industries           = $wc_calypso_bridge_filters->remove_not_allowed_industries( $industries );
-		$filtered_industries_variation = $wc_calypso_bridge_filters->remove_not_allowed_industries( $industries_variation );
+		$filtered_industries       = $wc_calypso_bridge_filters->remove_not_allowed_industries( $industries_slugs );
 		$this->assertEquals( count( $filtered_industries ), 2 );
-		$this->assertEquals( count( $filtered_industries_variation ), 2 );
 	}
 }
