422f5d21e7924ce6bc4c9df16aa5a89d161303c0
Moon <moon.kyong@automattic.com>
2021-02-18 17:06:36 -0800

    Change order link to wp-admin

--- store-on-wpcom/inc/wc-calypso-bridge-email-order-url.php
+++ store-on-wpcom/inc/wc-calypso-bridge-email-order-url.php
@@ -10,12 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function wc_calypso_bridge_email_get_wpcom_order_link( $url, $order ) {
-	$strip_http = '/.*?:\/\//i';
-	$site_slug  = preg_replace( $strip_http, '', get_home_url() );
-	$site_slug  = str_replace( '/', '::', $site_slug );
-	$order_id = $order->get_id();
-
-	return 'https://wordpress.com/store/order/' . $site_slug . '/' . $order_id;
+    return get_home_url() . '/wp-admin/post.php?post=' . $order->get_id() .'&action=edit';
 }
 
 add_filter( 'woocommerce_get_edit_order_url', 'wc_calypso_bridge_email_get_wpcom_order_link', 10, 2 );
