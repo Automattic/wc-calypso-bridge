04b7584416cb67665441a9084347c1d325496d72
Joel Thiessen <dygerati@gmail.com>
2021-02-24 13:51:50 -0800

    Basic build for compiling & live-reloading JS

--- class-wc-calypso-bridge.php
+++ class-wc-calypso-bridge.php
@@ -52,6 +52,8 @@ class WC_Calypso_Bridge {
 			return;
 		}
 		add_action( 'init', array( $this, 'load_ecommerce_plan_ui' ), 2 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'add_extension_register_script') );
+
 		$this->disable_powerpack_features();
 	}
 
@@ -128,6 +130,29 @@ class WC_Calypso_Bridge {
 		add_filter( 'get_user_metadata', array( $this, 'decalypsoify_ecommerce_plan' ), 10, 3 );
 		add_action( 'current_screen', array( $this, 'load_ui_elements' ) );
 
+	}
+
+	function add_extension_register_script() {
+		if ( ! class_exists( 'Automattic\WooCommerce\Admin\Loader' ) || ! \Automattic\WooCommerce\Admin\Loader::is_admin_or_embed_page() ) {
+			return;
+		}
+		
+		$script_path       = '/build/index.js';
+		$script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
+		$script_asset      = file_exists( $script_asset_path )
+			? require( $script_asset_path )
+			: array( 'dependencies' => array(), 'version' => filemtime( $script_path ) );
+		$script_url = plugins_url( $script_path, __FILE__ );
+	
+		wp_register_script(
+			'wc-calypso-bridge',
+			$script_url,
+			$script_asset['dependencies'],
+			$script_asset['version'],
+			true
+		);
+	
+		wp_enqueue_script( 'wc-calypso-bridge' );
 	}
 
 	/**
