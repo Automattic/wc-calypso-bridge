9d381479e48acd155a9638875df3c301ac062453
Ilyas Foo <foo.ilyas@gmail.com>
2021-06-23 10:25:47 +0800

    Remove Canada Post notice suppression and header fix (#685)
    * Remove Canada Post connect notice suppression
    
    * Fix for nav unification header bug on ecommerce plan users
    
    * Fix spacing
    
    * Fix indentation

--- class-wc-calypso-bridge.php
+++ class-wc-calypso-bridge.php
@@ -9,6 +9,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use Automattic\WooCommerce\Admin\Loader;
+
 /**
  * WC Calypso Bridge
  */
@@ -171,8 +173,10 @@ class WC_Calypso_Bridge {
 			add_filter( 'admin_footer_text', array( $this, 'update_woocommerce_footer' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'add_ecommerce_plan_styles' ) );
 
-			// Nav unification fixes.
-			if ( function_exists( 'wpcomsh_activate_nav_unification' ) && wpcomsh_activate_nav_unification( false ) ) {
+		// Nav unification fixes.
+		if ( function_exists( 'wpcomsh_activate_nav_unification' )
+			&& wpcomsh_activate_nav_unification( false )
+			&& ! Loader::is_feature_enabled( 'navigation' ) ) {
 				add_action( 'admin_enqueue_scripts', array( $this, 'add_nav_unification_styles' ) );
 			}
 		}
