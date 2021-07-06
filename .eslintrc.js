140196a2f288cf04cada6982efec929f3e75b900
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-07-06 09:03:23 +0800

    Add TypeScript support (#699)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,27 @@ module.exports = {
 			{ definedTags: [ 'jest-environment' ] },
 		],
 	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			parser: '@typescript-eslint/parser',
+			extends: [
+				'plugin:@woocommerce/eslint-plugin/recommended',
+				'plugin:@typescript-eslint/recommended',
+			],
+			rules: {
+				camelcase: 'off',
+				'import/no-unresolved': 'warn',
+				'import/no-extraneous-dependencies': 'warn',
+				'@typescript-eslint/no-explicit-any': 'error',
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-use-before-define': ['error'],
+				'jsdoc/require-param': 'off',
+				// Making use of typescript no-shadow instead, fixes issues with enum.
+				'no-shadow': 'off',
+				'@typescript-eslint/no-shadow': ['error'],
+				'@typescript-eslint/no-empty-function': 'off',
+			},
+		},
+	],
 };
