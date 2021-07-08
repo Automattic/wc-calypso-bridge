4eae6cbcff30fc3039a729240d90ae5200ed4e0e
Moon <moon.kyong@automattic.com>
2021-07-08 13:19:07 -0700

    Update FAQ content

--- src/payments-welcome/faq.tsx
+++ src/payments-welcome/faq.tsx
@@ -1,5 +1,19 @@
 const FrequentlyAskedQuestions = () => {
-	return <div>faq</div>;
+	return (
+		<>
+			<h2>Frequently asked questions</h2>
+			<h3>How will I save money using WooCommerce Payments?</h3>
+			<p>
+			Stores accepted into the promotional program will receive a 100% discount on transaction fees (excluding currency conversion fees) for the first $25,000 in payments, or 3 months, whichever comes first. Simply install the extension and if eligible you’ll be entered into the promotional offer. 
+			</p>
+			<p>
+			To be eligible for this promotional offer, your store must: (1) meet the WooCommerce Payments usage requirements; (2) be a U.S.-based business; (3) not have processed payments through WooCommerce Payments before; and (4) be accepted into the promotional program.
+			</p>
+			<h3>What will my customers see at checkout?</h3>
+			<h3>What happens to my data?</h3>
+			<h3>Where will my existing transactions show up?</h3>
+		</>
+	);
 };
 
 export default FrequentlyAskedQuestions;
