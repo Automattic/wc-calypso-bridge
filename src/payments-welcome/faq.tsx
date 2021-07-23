ec2058b04931f579266c520d367d865710ae0258
Ilyas Foo <foo.ilyas@gmail.com>
2021-07-23 10:33:26 +0800

    Fix faq html structure

--- src/payments-welcome/faq.tsx
+++ src/payments-welcome/faq.tsx
@@ -18,21 +18,19 @@ const FrequentlyAskedQuestions = () => {
 
 			<h3>{strings.faq.question2}</h3>
 			<p>{strings.faq.question2Answer1}</p>
-			<p>
-				<h4>{strings.faq.question2Answer2}</h4>
-				<ul>
-					<li>
-						{strings.faq.question2Answer3}
-						<ul>
-							<li>{strings.faq.question2Answer4}</li>
-							<li>{strings.faq.question2Answer5}</li>
-						</ul>
-					</li>
-					<li>{strings.faq.question2Answer6}</li>
-					<li>{strings.faq.question2Answer7}</li>
-				</ul>
-				{strings.faq.question2Answer8}
-			</p>
+			<h4>{strings.faq.question2Answer2}</h4>
+			<ul>
+				<li>
+					{strings.faq.question2Answer3}
+					<ul>
+						<li>{strings.faq.question2Answer4}</li>
+						<li>{strings.faq.question2Answer5}</li>
+					</ul>
+				</li>
+				<li>{strings.faq.question2Answer6}</li>
+				<li>{strings.faq.question2Answer7}</li>
+			</ul>
+			{strings.faq.question2Answer8}
 			<h3>{strings.faq.question3}</h3>
 			<p>{strings.faq.question3Answer1}</p>
 			<p>{strings.faq.question3Answer2}</p>
@@ -40,20 +38,18 @@ const FrequentlyAskedQuestions = () => {
 			<p>{strings.faq.question3Answer4}</p>
 			<p>{strings.faq.question3Answer5}</p>
 			<h3>{strings.faq.question4}</h3>
-			<p>
-				{strings.faq.question4Answer1}
-				<ul>
-					<li>{strings.faq.question4Answer2}</li>
-					<li>{strings.faq.question4Answer3}</li>
-					<li>{strings.faq.question4Answer4}</li>
-					<li>{strings.faq.question4Answer5}</li>
-					<li>{strings.faq.question4Answer6}</li>
-					<li>{strings.faq.question4Answer7}</li>
-					<li>{strings.faq.question4Answer8}</li>
-					<li>{strings.faq.question4Answer9}</li>
-					<li>{strings.faq.question4Answer10}</li>
-				</ul>
-			</p>
+			{strings.faq.question4Answer1}
+			<ul>
+				<li>{strings.faq.question4Answer2}</li>
+				<li>{strings.faq.question4Answer3}</li>
+				<li>{strings.faq.question4Answer4}</li>
+				<li>{strings.faq.question4Answer5}</li>
+				<li>{strings.faq.question4Answer6}</li>
+				<li>{strings.faq.question4Answer7}</li>
+				<li>{strings.faq.question4Answer8}</li>
+				<li>{strings.faq.question4Answer9}</li>
+				<li>{strings.faq.question4Answer10}</li>
+			</ul>
 			<p>{strings.faq.question4Answer11}</p>
 			<p>{strings.faq.question4Answer12}</p>
 			<p>{strings.faq.question4Answer13}</p>
