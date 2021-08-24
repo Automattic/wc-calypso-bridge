c3853d2621a9f24799ca9852b6238236e32e67eb
Louis Laugesen <louis.laugesen@gmail.com>
2021-08-24 16:10:39 +1000

    Add woop php base

---
+++ includes/woop.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Woo on Plans.
+ *
+ * @since 1.8.1
+ * @package WC_Calypso_Bridge
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Init woop.
+ *
+ * @return void
+ */
+function wc_calypso_bridge_woop_init() {
+	// Gate Woop behind feature flag
+	if ( ! defined( 'WPCOM_ENABLE_WOOP' ) ) {
+		return;
+	}
+
+	// Don't load mods in cronjobs.
+	if ( defined( 'DOING_CRON' ) ) {
+		return;
+	}
+
+	// Ensure a valid Woo install is installed.
+	if ( ! class_exists( 'woocommerce' ) || ! version_compare( get_option( 'woocommerce_db_version' ), WC_MIN_VERSION, '>=' ) ) {
+		return;
+	}
+
+	if ( is_admin() ) {
+		// require_once dirname( __FILE__ ) . '/woop/some-admin-feature.php';.
+	} else {
+		// require_once dirname( __FILE__ ) . '/woop/some-feature.php';.
+	}
+}
+add_action( 'plugins_loaded', 'wc_calypso_bridge_woop_init' );
---
+++ includes/yesno.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Yes / No Functions for use in WC option filters.
+ *
+ * @since 1.8.1
+ * @package WC_Calypso_Bridge
+ */
+
+/**
+ * Always return 'yes'.
+ *
+ * @return string yes
+ */
+function wc_calypso_bridge_return_yes() {
+	return 'yes';
+}
+
+/**
+ * Always returns 'no'.
+ *
+ * @return string no
+ */
+function wc_calypso_bridge_return_no() {
+	return 'no';
+}
