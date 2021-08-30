c289a6f2422c1e37e5d83114734c0310c7945e7c
Louis Laugesen <louis.laugesen@gmail.com>
2021-08-30 17:28:42 +1000

    Disable wc-admin redirects to the setup-wizard

--- includes/woop/hide-onboarding.php
+++ includes/woop/hide-onboarding.php
@@ -10,3 +10,9 @@ defined( 'ABSPATH' ) || exit;
 
 // Hide the 'WooCommerce Setup' card from wp-admin.
 add_filter( 'pre_option_woocommerce_task_list_hidden', 'wc_calypso_bridge_return_yes' );
+
+// Disable the setup wizard
+add_filter( 'woocommerce_enable_setup_wizard', '__return_false' );
+
+// Disable the setup wizard redirect on plugin activation
+add_filter( 'woocommerce_prevent_automatic_wizard_redirect', '__return_true' );
