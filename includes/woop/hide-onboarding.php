0f0a1a658a4ff70821a3c12a766e051bdd571a46
Louis Laugesen <louis.laugesen@gmail.com>
2021-09-01 17:36:55 +1000

    phpcs

--- includes/woop/hide-onboarding.php
+++ includes/woop/hide-onboarding.php
@@ -11,5 +11,5 @@ defined( 'ABSPATH' ) || exit;
 // Hide the 'WooCommerce Setup' card from wp-admin.
 add_filter( 'pre_option_woocommerce_task_list_hidden', 'wc_calypso_bridge_return_yes' );
 
-// Disable the setup wizard redirect on plugin activation
+// Disable the setup wizard redirect on plugin activation.
 add_filter( 'woocommerce_enable_setup_wizard', '__return_false' );
