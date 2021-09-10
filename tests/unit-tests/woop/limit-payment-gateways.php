e4fee559df1f22154b80c7791c90dc6795923767
Louis Laugesen <louis.laugesen@gmail.com>
2021-09-10 14:52:29 +1000

    Make sure class exists

--- tests/unit-tests/woop/limit-payment-gateways.php
+++ tests/unit-tests/woop/limit-payment-gateways.php
@@ -3,6 +3,8 @@
  * Tests.
  */
 
+class LimitPaymentGatewaysExample {}
+
 class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 	public function test_limits_strings_to_cod() {
 		$this->assertEquals(
@@ -18,14 +20,15 @@ class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 	}
 
 	public function test_does_not_limit_on_biz() {
+
 		Atomic_Plan_Manager::set_current_plan_slug( Atomic_Plan_Manager::BUSINESS_PLAN_SLUG );
 		$this->assertEquals(
 			[ 'WC_Gateway_COD', 'WC_Gateway_BACS' ],
 			wc_calypso_bridge_limit_payment_gateways( [ 'WC_Gateway_COD', 'WC_Gateway_BACS' ] )
 		);
 		$this->assertEquals(
-			[ new WC_Payment_Gateway_WCPay_Subscriptions_Compat() ],
-			wc_calypso_bridge_limit_payment_gateways( [ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ] )
+			[ new LimitPaymentGatewaysExample() ],
+			wc_calypso_bridge_limit_payment_gateways( [ new LimitPaymentGatewaysExample(), new \stdClass() ] )
 		);
 	}
 
@@ -36,8 +39,8 @@ class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 			wc_calypso_bridge_limit_payment_gateways( [ 'WC_Gateway_COD', 'WC_Gateway_BACS' ] )
 		);
 		$this->assertEquals(
-			[ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ],
-			wc_calypso_bridge_limit_payment_gateways( [ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ] )
+			[ new LimitPaymentGatewaysExample(), new \stdClass() ],
+			wc_calypso_bridge_limit_payment_gateways( [ new LimitPaymentGatewaysExample(), new \stdClass() ] )
 		);
 	}
 }
