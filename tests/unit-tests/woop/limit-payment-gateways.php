9f0b1ca65584592c5335450ec01fa77058a2bc09
Louis Laugesen <louis.laugesen@gmail.com>
2021-09-14 20:22:40 +1000

    test fixes

--- tests/unit-tests/woop/limit-payment-gateways.php
+++ tests/unit-tests/woop/limit-payment-gateways.php
@@ -3,15 +3,19 @@
  * Tests.
  */
 
-class LimitPaymentGatewaysExample {}
+if ( ! class_exists( 'WC_Payment_Gateway_WCPay_Subscriptions_Compat' ) ) {
+	class WC_Payment_Gateway_WCPay_Subscriptions_Compat {}
+}
 
 class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 	public function test_limits_strings_to_cod() {
+		Atomic_Plan_Manager::set_current_plan_slug( 'free' );
 		$this->assertEquals(
 			[ 'WC_Gateway_COD' ],
 			wc_calypso_bridge_limit_payment_gateways( [ 'WC_Gateway_COD', 'WC_Gateway_BACS' ] )
 		);
 	}
+
 	public function test_limits_classes_to_WCPay() {
 		$this->assertEquals(
 			[ new WC_Payment_Gateway_WCPay_Subscriptions_Compat() ],
@@ -20,15 +24,14 @@ class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 	}
 
 	public function test_does_not_limit_on_biz() {
-
 		Atomic_Plan_Manager::set_current_plan_slug( Atomic_Plan_Manager::BUSINESS_PLAN_SLUG );
 		$this->assertEquals(
 			[ 'WC_Gateway_COD', 'WC_Gateway_BACS' ],
 			wc_calypso_bridge_limit_payment_gateways( [ 'WC_Gateway_COD', 'WC_Gateway_BACS' ] )
 		);
 		$this->assertEquals(
-			[ new LimitPaymentGatewaysExample() ],
-			wc_calypso_bridge_limit_payment_gateways( [ new LimitPaymentGatewaysExample(), new \stdClass() ] )
+			[ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ],
+			wc_calypso_bridge_limit_payment_gateways( [ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ] )
 		);
 	}
 
@@ -39,8 +42,8 @@ class LimitPaymentGatewaysTest extends WC_Calypso_Bridge_Test {
 			wc_calypso_bridge_limit_payment_gateways( [ 'WC_Gateway_COD', 'WC_Gateway_BACS' ] )
 		);
 		$this->assertEquals(
-			[ new LimitPaymentGatewaysExample(), new \stdClass() ],
-			wc_calypso_bridge_limit_payment_gateways( [ new LimitPaymentGatewaysExample(), new \stdClass() ] )
+			[ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ],
+			wc_calypso_bridge_limit_payment_gateways( [ new WC_Payment_Gateway_WCPay_Subscriptions_Compat(), new \stdClass() ] )
 		);
 	}
 }
