75e5a6cb74efd5936b4158144e1d86031ec11ba8
Paul Sealock <psealock@gmail.com>
2021-10-14 14:53:42 +1300

    Adding build directory to release

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '628644329b012c0486c18caaa7c5598f');
