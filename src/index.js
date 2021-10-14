79353f393594d516d2c215b68e20b59c7b6095e3
Paul Sealock <psealock@gmail.com>
2021-10-14 13:56:25 +1300

    add patch CSS

--- src/index.js
+++ src/index.js
@@ -9,18 +9,19 @@ import { addFilter } from '@wordpress/hooks';
  */
 import wcNavFilterRootUrl from './wc-navigation-root-url';
 import PaymentsWelcomePage from './payments-welcome';
+import './index.scss';
 
 wcNavFilterRootUrl();
 
-addFilter('woocommerce_admin_pages_list', 'wc-calypso-bridge', (pages) => {
-	pages.push({
+addFilter( 'woocommerce_admin_pages_list', 'wc-calypso-bridge', ( pages ) => {
+	pages.push( {
 		container: PaymentsWelcomePage,
 		path: '/payments-welcome',
-		breadcrumbs: [__('WooCommerce Payments', 'wc-calypso-bridge')],
+		breadcrumbs: [ __( 'WooCommerce Payments', 'wc-calypso-bridge' ) ],
 		navArgs: {
 			id: 'wc-calypso-bridge-payments-welcome-page',
 		},
-	});
+	} );
 
 	return pages;
-});
+} );
