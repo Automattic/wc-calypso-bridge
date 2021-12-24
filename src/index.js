4f8ea9f3afac3bc0ec9d428f71b5bebeb3b43a7e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2021-12-24 11:20:45 +0800

    Remove wc-payment from index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 /**
- * External dependencies
- */
-import { __ } from '@wordpress/i18n';
-import { addFilter } from '@wordpress/hooks';
-
-/**
  * Internal dependencies
  */
 import wcNavFilterRootUrl from './wc-navigation-root-url';
-import PaymentsWelcomePage from './payments-welcome';
 import './index.scss';
 
 wcNavFilterRootUrl();
-
-addFilter( 'woocommerce_admin_pages_list', 'wc-calypso-bridge', ( pages ) => {
-	pages.push( {
-		container: PaymentsWelcomePage,
-		path: '/payments-welcome',
-		breadcrumbs: [ __( 'WooCommerce Payments', 'wc-calypso-bridge' ) ],
-		navArgs: {
-			id: 'wc-calypso-bridge-payments-welcome-page',
-		},
-	} );
-
-	return pages;
-} );
