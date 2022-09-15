dc80c914a816d50ce0a70939470e72a7a088639d
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2022-09-15 16:39:31 +0300

    👌 IMPROVE: Suppress coupon management has moved notes - Remove legacy coupon menu - #809

--- includes/class-wc-calypso-bridge-woocommerce-admin.php
+++ includes/class-wc-calypso-bridge-woocommerce-admin.php
@@ -3,8 +3,8 @@
  * Contains customizations for WooCommerce Admin
  *
  * @package WC_Calypso_Bridge/Classes
- * @since   1.0.17
- * @version 1.0.0
+ * @since   1.0.0
+ * @version 1.9.4
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -21,20 +21,24 @@ class WC_Calypso_Bridge_WooCommerce_Admin {
 	protected static $instance = false;
 
 	/**
-	 * Get class instance
+	 * Get class instance.
 	 */
 	public static function factory() {
 		if ( ! self::$instance ) {
 			self::$instance = new self();
 		}
+
 		return self::$instance;
 	}
 
 	/**
-	 * Initialize the class
+	 * Initialize the class.
 	 */
 	public function init() {
 		add_filter( 'wc_admin_get_feature_config', array( $this, 'maybe_remove_devdocs_menu_item' ) );
+
+		add_filter( 'pre_option_wc_admin_show_legacy_coupon_menu', array( $this, 'filter_show_legacy_coupon_menu' ), PHP_INT_MAX );
+		add_action( 'admin_init', array( $this, 'delete_coupon_moved_notes' ), PHP_INT_MAX );
 	}
 
 	/**
@@ -48,6 +52,38 @@ class WC_Calypso_Bridge_WooCommerce_Admin {
 		}
 
 		return $features;
+	}
+
+	/**
+	 * Remove the legacy `WooCommerce > Coupons` menu.
+	 *
+	 * @param mixed $value Value to be filtered.
+	 *
+	 * @return int 1 to show the legacy menu, 0 to hide it. Booleans do not work.
+	 * @see  Automattic\WooCommerce\Internal\Admin\CouponsMovedTrait::display_legacy_menu()
+	 * @todo Write a compatibility branch in CouponsMovedTrait to hide the legacy menu in new installations of WooCommerce.
+	 */
+	public function filter_show_legacy_coupon_menu( $value ) {
+		return 0;
+	}
+
+	/**
+	 * Delete all existing `Coupon Page Moved` notes from the DB.
+	 *
+	 * @return void
+	 * @todo Create a one-time operation controller, to delete all `wc-admin-coupon-page-moved` notes from the database.
+	 */
+	public function delete_coupon_moved_notes() {
+
+		if ( class_exists( 'Automattic\WooCommerce\Admin\Notes\Notes' ) ) {
+			$notes_class = 'Automattic\WooCommerce\Admin\Notes\Notes';
+		} elseif ( class_exists( 'Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes' ) ) {
+			$notes_class = 'Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes';
+		} else {
+			return;
+		}
+
+		$notes_class::delete_notes_with_name( 'wc-admin-coupon-page-moved' );
 	}
 }
 
