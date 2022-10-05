4150bdb8bc4b5e0611973af9c765b30b79031c59
Manos Psychogyiopoulos <psyx@somewherewarm.com>
2022-10-05 14:11:23 +0300

    Clean up product table columns

--- includes/class-wc-calypso-bridge-plugins.php
+++ includes/class-wc-calypso-bridge-plugins.php
@@ -43,6 +43,8 @@ class WC_Calypso_Bridge_Plugins {
 		add_filter( 'woocommerce_admin_onboarding_industries', array( $this, 'maybe_create_wc_pages' ), 10, 2 );
 		add_action( 'load-index.php', array( $this, 'maybe_remove_somewherewarm_maintenance_notices' ) );
 		add_action( 'load-plugins.php', array( $this, 'maybe_remove_somewherewarm_maintenance_notices' ) );
+		add_filter( 'manage_product_posts_columns', array( $this, 'remove_jetpack_stats_column' ), 100 );
+		add_filter( 'default_hidden_columns', array( $this, 'hide_product_columns' ), 100, 2 );
 	}
 
 	/**
@@ -178,6 +180,28 @@ class WC_Calypso_Bridge_Plugins {
 		}
 	}
 
+	/**
+	 * Removes the Stats column.
+	 *
+	 * @param  array $cols Array of product columns.
+	 * @return array
+	 */
+	public function remove_jetpack_stats_column( $cols ) {
+        return array_diff_key( $cols, array_flip( array( 'stats' ) ) );
+    }
+
+    /**
+	 * Hides the Likes and Date product columns by default.
+	 *
+	 * @param  array $cols Array of product columns.
+	 * @return array
+	 */
+    public function hide_product_columns( $hidden, $screen ) {
+    	if ( isset( $screen->id ) && 'edit-product' === $screen->id ) {
+        	return array_merge( $hidden, array( 'likes', 'date' ) );
+        }
+        return $hidden;
+    }
 }
 
 $wc_calypso_bridge_plugins = WC_Calypso_Bridge_Plugins::get_instance();
