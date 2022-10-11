8a0f0415fe9860e38ba3a3cacd4c8e513f2001a0
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2022-10-11 12:31:37 +0300

    ✨ Create an inbox message to let the users know about blocks #822

---
+++ includes/notes/class-wc-calypso-bridge-cart-checkout-blocks-default-inbox-note.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * WooCommerce Calypso Bridge
+ * Cart Checkout Blocks Inbox Note
+ *
+ * @package WC_Calypso_Bridge/Notes
+ * @since   1.9.5
+ * @version 1.9.5
+ */
+
+use Automattic\WooCommerce\Admin\Notes\Note;
+use Automattic\WooCommerce\Admin\Notes\NoteTraits;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WC_Calypso_Bridge_Cart_Checkout_Blocks_Default_Inbox_Note
+ */
+class WC_Calypso_Bridge_Cart_Checkout_Blocks_Default_Inbox_Note {
+	/**
+	 * Note traits.
+	 */
+	use NoteTraits;
+
+	/**
+	 * Name of the note for use in the database.
+	 */
+	const NOTE_NAME = 'wc-calypso-bridge-cart-checkout-blocks-default-inbox-note';
+
+	/**
+	 * Get the note.
+	 *
+	 * @return void|Note
+	 * @todo Update note content and when to display the inbox note.
+	 */
+	public static function get_note() {
+		if ( ! self::should_display_note() ) {
+			return;
+		}
+
+		$note = new Note();
+		$note->set_title( __( 'TODO: Default Cart/Checkout Blocks', 'wc-calypso-bridge' ) );
+		$note->set_content(
+			__(
+				'TODO:The new cart and checkout blocks are set by default. Be aware that not all plugins are compatible. You can still use the old blocks if you prefer.',
+				'woocommerce'
+			)
+		);
+		$note->set_content_data( (object) array() );
+		$note->set_type( Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+		$note->set_name( self::NOTE_NAME );
+		$note->set_source( 'woocommerce-admin' );
+		$note->add_action(
+			'learn-more',
+			__( 'Learn more', 'wc-calypso-bridge' ),
+			'https://woocommerce.com/document/managing-products/?utm_source=inbox&utm_medium=product'
+		);
+
+		return $note;
+	}
+
+	/**
+	 * Returns true if we should display the note.
+	 *
+	 * @return Boolean
+	 * @todo Set the correct Blocks version, after fixes have been released.
+	 * @see  https://somewherewarmattic.wordpress.com/2022/10/07/enabling-cart-checkout-blocks-on-atomic-on-hold/
+	 */
+	public static function should_display_note() {
+
+		if (
+			class_exists( 'Automattic\WooCommerce\Blocks\Package' )
+			&& version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '7.0.0' ) > 0
+			&& self::wc_admin_active_for( 2 * DAY_IN_SECONDS )
+		) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Delete note if we shouldn't display it and not been actioned on.
+	 *
+	 * @return void
+	 */
+	public static function possibly_clear_note() {
+		if ( ! self::should_display_note() && ! self::has_note_been_actioned() ) {
+			self::possibly_delete_note();
+		}
+	}
+
+}
