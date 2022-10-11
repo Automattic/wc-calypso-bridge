938c8c94267be70a31e2bd31c7629d3d4eaf347e
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2022-10-11 14:07:36 +0300

    Adding build directory to release

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '82f9676bbcf48f37dd8e5d4eeaaa143a');
