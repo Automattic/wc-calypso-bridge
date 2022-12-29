fd83361d0977c4de108d491518300e2b6e5f9d47
“Chris <xristos3490@gmail.com>
2022-12-29 11:38:15 +0200

    Remove jetpack sync whitelist options

--- store-on-wpcom/class-wc-calypso-bridge.php
+++ store-on-wpcom/class-wc-calypso-bridge.php
@@ -73,7 +73,6 @@ class WC_Calypso_Bridge {
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-enable-auto-update-db.php';
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-hide-alerts.php';
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-jetpack-hotfixes.php';
-		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-jetpack-sync.php';
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-mailchimp-deactivate-hook.php';
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-mailchimp-no-redirect.php';
 		include_once dirname( __FILE__ ) . '/inc/wc-calypso-bridge-masterbar-menu.php';
