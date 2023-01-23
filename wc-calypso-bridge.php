2796788c83f177413c06b089af47b10c8b9d8cc9
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2023-01-23 14:07:43 +0200

    🔖 Prepare to release 1.9.17

--- wc-calypso-bridge.php
+++ wc-calypso-bridge.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Calypso Bridge
  * Plugin URI: https://wordpress.com/
  * Description: A feature plugin to provide ux enhancments for users of Store on WordPress.com.
- * Version: 1.9.16
+ * Version: 1.9.17
  * Author: Automattic
  * Author URI: https://wordpress.com/
  * Requires at least: 4.4
@@ -37,7 +37,7 @@ if ( file_exists( WP_PLUGIN_DIR . '/wc-calypso-bridge/wc-calypso-bridge.php' ) )
 }
 
 define( 'WC_CALYSPO_BRIDGE_PLUGIN_FILE', __FILE__ );
-define( 'WC_CALYPSO_BRIDGE_CURRENT_VERSION', '1.9.16' );
+define( 'WC_CALYPSO_BRIDGE_CURRENT_VERSION', '1.9.17' );
 define( 'WC_MIN_VERSION', '3.0.0' );
 
 if ( ! function_exists( 'wc_calypso_bridge_is_ecommerce_plan' ) ) {
