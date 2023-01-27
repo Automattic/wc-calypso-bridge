fb208db0ebdaac5765e64c5798925740aa6c5d31
“Chris <xristos3490@gmail.com>
2023-01-27 18:36:53 +0200

    Fix some issues in hide-alerts and remove it from business

--- includes/class-wc-calypso-bridge-hide-alerts.php
+++ includes/class-wc-calypso-bridge-hide-alerts.php
@@ -38,7 +38,12 @@ class WC_Calypso_Bridge_Hide_Alerts {
 	 * Constructor.
 	 */
 	private function __construct() {
-		// Both ecommerce and business.
+
+		// Only in Ecommerce.
+		if ( ! wc_calypso_bridge_is_ecommerce_plan() ) {
+			return;
+		}
+
 		add_action( 'init', array( $this, 'init' ) );
 
 		/**
@@ -130,7 +135,7 @@ class WC_Calypso_Bridge_Hide_Alerts {
 	 * Prevents some alerts like the Apple Pay alert and Akismet from being shown on pages besides settings pages / core wp-admin pages.
 	 */
 	public function hide_alerts_on_non_settings_pages() {
-		if ( empty( $_GET['page'] || 'wc-settings' !== $_GET['page'] ) ) {
+		if ( empty( $_GET['page'] ) || 'wc-settings' !== $_GET['page'] ) {
 			WC_Calypso_Bridge_Helper_Functions::remove_class_action( 'admin_notices', 'WC_Stripe_Apple_Pay_Registration', 'admin_notices', 10 );
 			remove_action( 'admin_notices', array( 'Akismet_Admin', 'display_notice' ) );
 		}
