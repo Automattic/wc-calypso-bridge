441ee4cdf21357baa1cf4341737c9005d5ee7807
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2023-01-30 18:28:42 +0200

    👌 Stripe, WC Payments, PayPal restrictions are in place

--- includes/class-wc-calypso-bridge-free-trial.php
+++ includes/class-wc-calypso-bridge-free-trial.php
@@ -39,18 +39,141 @@ class WC_Calypso_Bridge_Free_Trial {
 	 * Constructor.
 	 */
 	public function __construct() {
-		add_action( 'woocommerce_init', array( $this, 'init' ), 20 );
-	}
 
-	/**
-	 * Free trial hooks.
-	 */
-	public function init() {
-
-		// Bail out early if the site is not in free trial mode.
 		if ( ! $this->is_free_trial() ) {
 			return;
 		}
+
+		add_action( 'woocommerce_init', array( $this, 'frontend' ), PHP_INT_MAX );
+		add_action( 'admin_init', array( $this, 'backend' ), PHP_INT_MAX );
+
+		// Disable Stripe Express buttons.
+		add_filter( 'pre_option_woocommerce_stripe_settings', '__return_false', PHP_INT_MAX );
+		add_filter( 'option_woocommerce_stripe_settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['payment_request']                  = 'no';
+			$value['payment_request_button_locations'] = array();
+
+			return $value;
+		}, PHP_INT_MAX );
+
+		add_filter( 'wc_stripe_hide_payment_request_on_product_page', function ( $value, $post ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			return false;
+		}, PHP_INT_MAX, 2 );
+
+		add_filter( 'wc_stripe_show_payment_request_on_cart', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			return false;
+		}, PHP_INT_MAX );
+
+		add_filter( 'wc_stripe_show_payment_request_on_checkout', function ( $value, $post ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			return false;
+		}, PHP_INT_MAX, 2 );
+
+		// Disable WooCommerce Payment Express buttons. TODO: Wasn't able to test this.
+		add_filter( 'pre_option_woocommerce_woocommerce_payments_settings', '__return_false', PHP_INT_MAX );
+		add_filter( 'option_woocommerce_woocommerce_payments_settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['payment_request']                  = 'no';
+			$value['payment_request_button_locations'] = array();
+
+			return $value;
+		}, PHP_INT_MAX );
+
+		/****** PAYPAL Express Checkout ******/
+		add_filter( 'pre_option_woocommerce-ppcp-settings', '__return_false', PHP_INT_MAX );
+		// There is no need for a `default_option` filter, as the default value is an empty array.
+		add_filter( 'option_woocommerce-ppcp-settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['button_product_enabled']      = false;
+			$value['button_cart_enabled']         = false;
+			$value['button_mini-cart_enabled']    = false;
+			$value['pay_later_button_enabled']    = false;
+			$value['pay_later_messaging_enabled'] = false;
+
+			return $value;
+		}, PHP_INT_MAX );
+
+		// TODO: Lets discuss; Should we completely disable PayPal for non-admin users on free trial. Thoughts?
+		add_filter( 'pre_option_woocommerce_ppcp-gateway_settings', '__return_false', PHP_INT_MAX );
+		add_filter( 'option_woocommerce_ppcp-gateway_settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['enabled'] = 'no';
+
+			return $value;
+		}, PHP_INT_MAX );
+
+		// TODO: Lets discuss, I found this paypal oxxo gateway. Should we disable it? It's part of PayPal's gateway codebase.
+		add_filter( 'pre_option_woocommerce_ppcp-oxxo-gateway_settings', '__return_false', PHP_INT_MAX );
+		add_filter( 'option_woocommerce_ppcp-oxxo-gateway_settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['enabled'] = 'no';
+
+			return $value;
+		}, PHP_INT_MAX );
+
+		// TODO: Lets discuss, I found this paypal pay upon invoice gateway. Should we disable it? It's part of PayPal's gateway codebase.
+		add_filter( 'pre_option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', '__return_false', PHP_INT_MAX );
+		add_filter( 'option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', function ( $value ) {
+
+			// Bail out early if the current user is allowed to create orders on free trial.
+			if ( current_user_can( 'manage_woocommerce' ) ) {
+				return $value;
+			}
+
+			$value['enabled'] = 'no';
+
+			return $value;
+		}, PHP_INT_MAX );
+
+	}
+
+	/**
+	 * Frontend free trial hooks.
+	 */
+	public function frontend() {
 
 		// Bail out early if the current user is allowed to create orders on free trial.
 		if ( current_user_can( 'manage_woocommerce' ) ) {
@@ -78,17 +201,24 @@ class WC_Calypso_Bridge_Free_Trial {
 			);
 		}, PHP_INT_MAX );
 
-		// Display notice on the checkout page
+		// Display an info message on the checkout page.
 		add_filter( 'the_content', function ( $content ) {
 			if ( ! is_checkout() ) {
 				return $content;
 			}
 
 			$message = __( 'TODO: Only Admins and Shop Managers can place test orders. Contact an Admin to get permission.', 'wc-calypso-bridge' );
-			$markup  = '<div class="woocommerce"><div class="woocommerce-notices-wrapper"><ul class="woocommerce-error" role="alert"><li> ' . $message . '</li></ul></div></div>';
+			$markup  = '<div class="woocommerce"><div class="woocommerce-notices-wrapper"><ul class="woocommerce-info role="alert"><li> ' . $message . '</li></ul></div></div>';
 
 			return $markup . $content;
 		}, PHP_INT_MAX );
+
+	}
+
+	/**
+	 * Backend free trial hooks.
+	 */
+	public function backend() {
 
 		// Display notice in Admin > Payments settings.
 		add_action( 'admin_notices', function () {
@@ -118,59 +248,17 @@ class WC_Calypso_Bridge_Free_Trial {
 			$plan_url  = 'https://wordpress.com/plans/' . $site_slug;
 			$message   = sprintf( __( 'TODO: During trial, only Admins and Shop Managers can place orders. To process real transactions, <a href="%s">pick a plan</a>.', 'wc-calypso-bridge' ), $plan_url );
 			?>
-			<div class="notice notice-warning">
+			<div class="notice notice-info">
 				<p><?php echo $message; ?></p>
 			</div>
 			<?php
-		}, PHP_INT_MAX );
-
-		/****** PAYPAL ******/
-
-		add_filter( 'pre_option_woocommerce-ppcp-settings', '__return_false', PHP_INT_MAX );
-		// There is no need for a `default_option` filter, as the default value is an empty array.
-		add_filter( 'option_woocommerce-ppcp-settings', function ( $value ) {
-			$value['button_product_enabled']      = false;
-			$value['button_cart_enabled']         = false;
-			$value['button_mini-cart_enabled']    = false;
-			$value['pay_later_button_enabled']    = false;
-			$value['pay_later_messaging_enabled'] = false;
-
-			return $value;
-		}, PHP_INT_MAX );
-
-		// Alternatively, we can completely disable PayPal for non-admin users on free trial.
-		add_filter( 'pre_option_woocommerce_ppcp-gateway_settings', '__return_false', PHP_INT_MAX );
-		add_filter( 'option_woocommerce_ppcp-gateway_settings', function ( $value ) {
-			$value['enabled'] = 'no';
-
-			return $value;
-		}, PHP_INT_MAX );
-
-		add_filter( 'pre_option_woocommerce_ppcp-oxxo-gateway_settings', '__return_false', PHP_INT_MAX );
-		add_filter( 'option_woocommerce_ppcp-oxxo-gateway_settings', function ( $value ) {
-			$value['enabled'] = 'no';
-
-			return $value;
-		}, PHP_INT_MAX );
-
-		add_filter( 'pre_option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', '__return_false', PHP_INT_MAX );
-		add_filter( 'option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', function ( $value ) {
-			$value['enabled'] = 'no';
-
-			return $value;
-		}, PHP_INT_MAX );
-
-		add_filter( 'pre_option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', '__return_false', PHP_INT_MAX );
-		add_filter( 'option_woocommerce_ppcp-pay-upon-invoice-gateway_settings', function ( $value ) {
-			$value['enabled'] = 'no';
-
-			return $value;
 		}, PHP_INT_MAX );
 
 	}
 
 	/**
 	 * @return boolean Whether the site is in free trial mode.
+	 * @todo Properly check if free trial in enabled.
 	 */
 	public function is_free_trial() {
 		return true;
