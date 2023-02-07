e6a6afce769457c3221ba8c2bb486e22cfbf6f93
moon <moon.kyong@automattic.com>
2023-02-07 15:34:31 -0800

    Add Typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
+const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
 
 module.exports = {
 	...defaultConfig,
 	plugins: [
 		...defaultConfig.plugins.filter(
-			(plugin) =>
+			( plugin ) =>
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 		new WooCommerceDependencyExtractionWebpackPlugin(),
-	]
+		new ForkTsCheckerWebpackPlugin(),
+	],
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.tsx?$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [ '.json', '.js', '.jsx', '.ts', '.tsx' ],
+	},
 };
