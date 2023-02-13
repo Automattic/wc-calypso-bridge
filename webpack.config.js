e62054d6433d109f87c82b047a90812c66f38c53
moon <moon.kyong@automattic.com>
2023-02-13 13:40:17 -0800

    Remove TS setup -- @wordpress/scripts > 20.0.0 includes TS setup by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,13 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
-const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
 
 module.exports = {
 	...defaultConfig,
 	plugins: [
 		...defaultConfig.plugins.filter(
-			( plugin ) =>
+			(plugin) =>
 				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 		new WooCommerceDependencyExtractionWebpackPlugin(),
-		new ForkTsCheckerWebpackPlugin(),
-	],
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-			{
-				test: /\.tsx?$/,
-				use: 'babel-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: [ '.json', '.js', '.jsx', '.ts', '.tsx' ],
-	},
+	]
 };
