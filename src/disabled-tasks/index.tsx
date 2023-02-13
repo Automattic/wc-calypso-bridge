857648822d0230844c4426093784cc5d6a6370e5
moon <moon.kyong@automattic.com>
2023-02-13 14:36:43 -0800

    Use __ for the label

--- src/disabled-tasks/index.tsx
+++ src/disabled-tasks/index.tsx
@@ -17,22 +17,18 @@ export const DisabledTasks = () => {
 		"You're currently using a free trial! To get access to the full range of features, please sign up for a plan.",
 		'wc-calypso-bridge'
 	);
+	const listLabel = __(
+		'Upgrade to paid plan to unlock the next tasks',
+		'wc-calypso-bridge'
+	);
 	const signupUrl =
 		'https://wordpress.com/plans/' + window.wcCalypsoBridge.siteSlug;
 	return (
 		<ExperimentalCollapsibleList
-			collapseLabel="Upgrade to paid plan to unlock the next tasks"
-			expandLabel="Upgrade to paid plan to unlock the next tasks"
+			collapseLabel={ listLabel }
+			expandLabel={ listLabel }
 			show={ 0 }
 			collapsed={ true }
-			onCollapse={ () => {
-				// eslint-disable-next-line no-console
-				console.log( 'collapsed' );
-			} }
-			onExpand={ () => {
-				// eslint-disable-next-line no-console
-				console.log( 'expanded' );
-			} }
 			direction="bottom"
 			className="free-trial-disabled-tasks"
 		>
