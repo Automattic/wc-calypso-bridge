5c4b0550fd27360f32ad3ff111a5abd39d957e9e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-02-23 11:43:05 +0800

    Add a patch to fix useSlot when using Gutenberg 14.3+

---
+++ src/use-slot-patch.js
@@ -0,0 +1,29 @@
+'use strict';
+/* This file is a patch to override the useSlot hook in the @woocommerce/experimental package.
+ * This is a temporary fix until the WooCommerce updated to 7.5.0.
+ */
+
+const wpUseSlotFills = window.wp.components.__experimentalUseSlotFills;
+const wpUseSlot = window.wp.components.__experimentalUseSlot;
+
+// Override the useSlot hook only if the experimental useSlotFills hook is available (Gutenberg 14.3+).
+if ( typeof wpUseSlotFills === 'function' ) {
+	// We need to re-assign the useSlot hook to the experimental object because it doesn't allow us to redefine property directly.
+	window.wc.experimental = {
+		...window.wc.experimental,
+		useSlot: ( name ) => {
+			const slot = wpUseSlot( name );
+			const fills = wpUseSlotFills( name );
+			return {
+				...slot,
+				fills,
+			};
+		},
+	};
+
+	// Make the useSlot hook non-configurable and non-writable.
+	Object.defineProperty( window.wc.experimental, 'useSlot', {
+		configurable: false,
+		writable: false,
+	} );
+}
