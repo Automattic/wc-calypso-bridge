c794f8760e94362e3e354a906cfa2540bfe499fb
Moon <moon.kyong@automattic.com>
2023-02-27 19:31:41 -0800

    Update class-wc-calypso-bridge.php
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>

--- class-wc-calypso-bridge.php
+++ class-wc-calypso-bridge.php
@@ -124,7 +124,6 @@ class WC_Calypso_Bridge {
 		require_once WC_CALYPSO_BRIDGE_PLUGIN_PATH . '/includes/free-trial/class-wc-calypso-bridge-free-trial-orders-changes.php';
 		require_once WC_CALYPSO_BRIDGE_PLUGIN_PATH . '/includes/free-trial/class-wc-calypso-bridge-free-trial-wc-payments.php';
 		require_once WC_CALYPSO_BRIDGE_PLUGIN_PATH . '/includes/class-wc-calypso-bridge-frontend-free-trial.php';
-
 	}
 
 	/**
