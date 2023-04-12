165f57c68bad3b3a64f7e457f7b6ee036857f246
Moon <moon.kyong@automattic.com>
2023-04-12 13:21:40 -0700

    Update includes/tasks/class-wc-calypso-task-free-trial-store-details.php
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>

--- includes/tasks/class-wc-calypso-task-free-trial-store-details.php
+++ includes/tasks/class-wc-calypso-task-free-trial-store-details.php
@@ -7,8 +7,8 @@ use Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\StoreDetails;
 /**
  * TrialStoreDetails Task
  * 
- * @since   2.0.14
- * @version 2.0.14
+ * @since   2.0.15
+ * @version 2.0.15
  */
 class TrialStoreDetails extends StoreDetails {
 	/**
