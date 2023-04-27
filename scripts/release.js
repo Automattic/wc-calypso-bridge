31421412a4708fff9f8e9581208c5614065d891a
Mark Biek <mark.biek@automattic.com>
2023-04-27 14:58:49 -0400

    First pass at a command to do a release build

--- scripts/release.js
+++ scripts/release.js
@@ -1,4 +1,41 @@
-const simpleGit = require('simple-git');
-const git = simpleGit();
+import chalk from 'chalk';
 
-console.log('Coming Soon!');
+import buildRelease from './commands/build.js';
+import {
+	error,
+	info,
+	isCorrectNodeVersion,
+	getCurrentVersion,
+	getNvmrcVersion,
+	promptContinue,
+} from './utils.js';
+
+async function main() {
+	if ( ! isCorrectNodeVersion() ) {
+		error(
+			`Your version of NodeJS is not correct. Please install NodeJS v${ getNvmrcVersion() }.`
+		);
+		return;
+	}
+
+	const version = getCurrentVersion();
+	let res = null;
+
+	const shouldContinue = await promptContinue(
+		`The current version is ${ chalk.blue(
+			version
+		) }. Would you like to create a new release?`
+	);
+
+	if ( ! shouldContinue ) {
+		info( 'Aborting release build.' );
+		return;
+	}
+
+	res = await buildRelease();
+	if ( ! res ) {
+		return;
+	}
+}
+
+main();
