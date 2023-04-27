cd96810c1a34760bbf90b1819bdfce2f7da7b087
Mark Biek <mark.biek@automattic.com>
2023-04-27 14:58:44 -0400

    Utility output functions

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,17 +1,46 @@
-const fs = require('fs');
+import fs from 'fs';
+import simpleGit from 'simple-git';
+import chalk from 'chalk';
 
-function getCurrentVersion() {
+const git = simpleGit();
+
+export function error( message ) {
+	console.log( 'ERROR: ' + chalk.red( message ) );
+}
+
+export function success( message ) {
+	console.log( 'SUCCESS: ' + chalk.green( message ) );
+}
+
+export function warning( message ) {
+	console.log( 'WARNING: ' + chalk.yellow( message ) );
+}
+
+export function info( message ) {
+	console.log( 'INFO: ' + chalk.blue( message ) );
+}
+
+export function getCurrentVersion() {
 	try {
-		const packageJson = fs.readFileSync('../composer.json');
-		const { version } = JSON.parse(packageJson);
-	
+		const packageJson = fs.readFileSync( '../composer.json' );
+		const { version } = JSON.parse( packageJson );
+
 		return version;
-	} catch (error) {
-		console.error(error);
+	} catch ( error ) {
+		console.error( error );
 		return null;
 	}
 }
 
-module.exports = {
-	getCurrentVersion
+export async function getStatus() {
+	try {
+		const status = await git.status();
+
+		return status;
+	} catch ( err ) {
+		console.error( err );
+		console.log(
+			'Invalid path. Usage: node script.js /path/to/repository'
+		);
+	}
 }
