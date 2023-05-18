5b9a6ebf29b85144b9e33c66d07cb1c7134918ce
Mark Biek <mark.biek@automattic.com>
2023-05-18 13:59:39 -0400

    fixup! WIP: prep-release command

--- scripts/prep-release.js
+++ scripts/prep-release.js
@@ -16,6 +16,17 @@ async function main() {
 			NOTICE_LEVEL.ERROR
 		);
 	}
+
+	info( `This command will prepare a new wc-calypso-bridge release with the following steps:
+	1. Bump the version number in the composer.json file and the wc-calypso-bridge.php file.
+	2. Update the changelog in readme.txt.
+	3. Update the translation files (optional).
+	4. Create a new PR with the changes.
+	` );
+
+	if ( ! ( await promptContinue( 'Continue? (y/N)' ) ) ) {
+		process.exit( 1 );
+	}
 }
 
 main();
