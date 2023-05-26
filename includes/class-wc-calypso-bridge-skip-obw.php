ded145bfae81db60ff9343abb100576b4db9f841
Dale du Preez <dale@automattic.com>
2023-05-26 11:51:46 +0200

    Default in an array when the option is corrupted

--- includes/class-wc-calypso-bridge-skip-obw.php
+++ includes/class-wc-calypso-bridge-skip-obw.php
@@ -77,13 +77,14 @@ class WC_Calypso_Bridge_Skip_OBW {
 	 */
 	public function add_skipped_state( $option_value ) {
 		$value = $option_value ?? array();
-		if ( is_array( $value ) ) {
-			$value['skipped'] = true;
-		} elseif ( empty( $value ) ) {
-			$value = array(
-				'skipped' => true,
-			);
+
+		if ( ! is_array( $value ) ) {
+			// If we don't have an array, return a valid option value.
+			$value = array();
 		}
+
+		$value['skipped'] = true;
+
 		return $value;
 	}
 }
