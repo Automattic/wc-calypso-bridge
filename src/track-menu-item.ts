19205f576ba3d54f474859a3adcd9b7e7df063f8
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-05-26 11:12:20 +0800

    Update src/track-menu-item.ts
    Co-authored-by: daledupreez <dale@automattic.com>

--- src/track-menu-item.ts
+++ src/track-menu-item.ts
@@ -5,11 +5,22 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 
 document.addEventListener( 'DOMContentLoaded', function () {
 	// Track clicks on the "Upgrades > Plans" menu item in the sidebar
-	const el = document.querySelector(
-		'#toplevel_page_paid-upgrades > ul > li.wp-first-item'
+	// Find all links under Upgrades that point to /plans/* paths
+	const possiblePlansLinks = document.querySelectorAll(
+		'#toplevel_page_paid-upgrades a[href^="https://wordpress.com/plans/"]'
 	);
-	if ( el ) {
-		el.addEventListener( 'click', function () {
+	possiblePlansLinks.forEach( ( possiblePlanLink ) => {
+		possiblePlanLink.addEventListener( 'click', function ( evt ) {
+			const href = evt && evt.target ? evt.target.getAttribute( 'href' ) : null;
+			if ( ! href || typeof href !== 'string' || ! href.startsWith( 'https://wordpress.com/plans/' )  ) {
+				return;
+			}
+			// Check if we're navigating to /plans/:siteSlug or some deeper path below /plans/[something]/siteSlug
+			// The implementation can be changed to be simpler or different, but the check is needed.
+			const hasDeeperPath = href.substring( 28 ).includes( '/' );
+			if ( hasDeeperPath ) {
+				return;
+			}
 			recordTracksEvent( 'calypso_sidebar_item_click', {
 				path: '/plans',
 			} );
