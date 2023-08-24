2facc1b71c7465fe5094f330455f0dedeb594497
Moon <moon.kyong@automattic.com>
2023-08-24 03:39:07 -0700

    Convert plugins page to a WC page (#1278)
    * Add page_view and plugins_view tracks
    
    * Use WC_Tracks::recordEvent to append WC properties
    
    * Use !important to make sure the styles do not get overridden by WC CSS
    
    * Remove PHP rendering code
    
    * Remove PHP template
    
    * Add JS side plugins page
    
    * Update includes/free-trial/class-wc-calypso-bridge-free-trial-plugins-screen.php
    
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>
    
    * Update includes/free-trial/class-wc-calypso-bridge-free-trial-plugins-screen.php
    
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>
    
    * Change breadcrumb to Plugins
    
    * Add plugins_view track and fix CSS
    
    * Update changelog
    
    * Remove redundant track
    
    ---------
    
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>

--- includes/free-trial/class-wc-calypso-bridge-free-trial-plugins-screen.php
+++ includes/free-trial/class-wc-calypso-bridge-free-trial-plugins-screen.php
@@ -47,32 +47,25 @@ class WC_Calypso_Bridge_Free_Trial_Plugins_Screen {
 	 */
 	protected function init() {
 		add_action( 'admin_menu', array($this, 'add_menu_page'));
-		add_filter( 'admin_body_class', function( $classes ) {
-				$screen = get_current_screen();
-				if ( $screen && 'toplevel_page_plugins-upgrade' === $screen->id ) {
-					$classes .= ' woocommerce_page_wc-bridge-landing-page woocommerce_page_wc-plugins-landing-page ';
-				}
-
-				return $classes;
-			} );
 	}
 
 	/**
 	 * Initialize hooks.
 	 */
 	public function add_menu_page() {
-		add_menu_page( __( 'Plugins', 'wc-calypso-bridge' ), __( 'Plugins', 'wc-calypso-bridge' ), 'manage_options', 'plugins-upgrade', array( $this, 'output' ), 'dashicons-admin-plugins', 65 );
-	}
-
-	public function output() {
-		$upgrade_url = sprintf( 'https://wordpress.com/plans/%s', WC_Calypso_Bridge_Instance()->get_site_slug() );
-
-		/**
-		 * Addon page view.
-		 *
-		 * @uses $upgrade_url
-		 */
-		include_once WC_CALYPSO_BRIDGE_PLUGIN_PATH . '/includes/templates/html-admin-page-plugins-landing-page.php';
+		wc_admin_register_page(
+			array(
+				'id'        => 'woocommerce-wccom-plugins',
+				'title'     => __( 'Plugins', 'wc-calypso-bridge' ),
+				'nav_args'  => array(
+					'title'  => __( 'Plugins', 'wc-calypso-bridge' ),
+				),
+				'path'=> '/plugins-upgrade',
+				'position'  => 65,
+				'icon'=>'dashicons-admin-plugins',
+				'capability' => 'manage_options',
+			)
+		);
 	}
 }
 
