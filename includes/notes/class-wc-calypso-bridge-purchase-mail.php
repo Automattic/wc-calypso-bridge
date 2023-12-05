0327fccea176c391df4f3e2c18130068486d7e13
Jason Kytros <jason.kytros@automattic.com>
2023-12-05 14:42:01 +0200

    Add placeholder class for Professional Email inbox note

---
+++ includes/notes/class-wc-calypso-bridge-purchase-mail.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WooCommerce Calypso Bridge
+ * Choose Domain Note
+ *
+ * @package WC_Calypso_Bridge/Notes
+ * @since   x.x.x
+ * @version x.x.x
+ */
+
+use Automattic\WooCommerce\Admin\Notes\Note;
+use Automattic\WooCommerce\Admin\Notes\NoteTraits;
+use Automattic\WooCommerce\Admin\WCAdminHelper;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WC_Calypso_Bridge_Purchase_Mail_Note
+ */
+class WC_Calypso_Bridge_Purchase_Mail_Note {
+	
+}
