0ee7cf2fae8bb697087db8791e8bbbb27121d743
Moon <moon.kyong@automattic.com>
2024-01-31 20:45:27 -0800

    Update includes/free-trial/partners/class-wc-calypso-bridge-partner-square.php
    Co-authored-by: Ilyas Foo <foo.ilyas@gmail.com>

--- includes/free-trial/partners/class-wc-calypso-bridge-partner-square.php
+++ includes/free-trial/partners/class-wc-calypso-bridge-partner-square.php
@@ -3,8 +3,8 @@
 /**
  * WC Calypso Bridge Partner Square
  *
- *	@since   2.3.4
- *	@version 2.3.4
+ *	@since   x.x.x
+ *	@version x.x.x
 
  * This file includes customizations for the sites that were created through /start/square on woo.com.
  * woocommerce_onboarding_profile.partner must get 'square'
