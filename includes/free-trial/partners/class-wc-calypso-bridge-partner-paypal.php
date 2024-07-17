430ad630d532a30d49aa93782f29f1ef16253559
Roberto Aranda <roberto.aranda@automattic.com>
2024-07-17 20:19:25 +0200

    Remove default Get Paid task from the onboarding steps

--- includes/free-trial/partners/class-wc-calypso-bridge-partner-paypal.php
+++ includes/free-trial/partners/class-wc-calypso-bridge-partner-paypal.php
@@ -82,17 +82,27 @@ class WC_Calypso_Bridge_Partner_PayPal {
      */
     private function add_paypal_setup_task() {
         add_filter( 'woocommerce_admin_experimental_onboarding_tasklists', function( $lists ) {
-            if ( !$this->has_paypal_plugin_class() ){
-                return $lists;
-            }
-
-            if ( isset( $lists['setup'] ) ) {
+			if ( isset( $lists['setup'] ) ) {
                 require_once __DIR__ . '/../../tasks/class-wc-calypso-task-get-paid-with-paypal.php';
-                // Place it at the third position.
-                array_splice( $lists['setup']->tasks, 2, 0, array( new \Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\WCBridgeGetPaidWithPayPal( $lists['setup'] ) ) );
-            }
-            return $lists;
-        } );
+
+				$removeTasks = [
+					'Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\TrialPayments',
+					'Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\WooCommercePayments'
+				];
+
+				$lists['setup']->tasks = array_filter( $lists['setup']->tasks,  function( $task ) use ($removeTasks) {
+					if ( in_array( get_class( $task ), $removeTasks ) ) {
+						return false;
+					}
+
+					return true;
+				});
+
+				// Place it at the third position.
+				array_splice( $lists['setup']->tasks, 2, 0, array( new \Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\WCBridgeGetPaidWithPayPal( $lists['setup'] ) ) );
+			}
+			return $lists;
+		} );
     }
 
     /**
