62a5281f726a3b5d0619a0b689ecb983abcb5380
Boro Sitnikovski <boro.sitnikovski@automattic.com>
2026-01-20 19:28:34 +0100

    Refactor method to hide WooCommerce tracking settings
    Fixes WOOPLUG-6150
    
    Refactor the method to hide WooCommerce.com tracking settings by filtering out specific settings based on their IDs.
    
    Make sure to test both instructions in https://github.com/Automattic/wc-calypso-bridge/pull/1564 and [WOOPLUG-6150: Unable to disable usage tracking on Dotcom hosted sites](https://linear.app/a8c/issue/WOOPLUG-6150/unable-to-disable-usage-tracking-on-dotcom-hosted-sites)

--- includes/class-wc-calypso-bridge-tracks.php
+++ includes/class-wc-calypso-bridge-tracks.php
@@ -90,6 +90,9 @@ class WC_Calypso_Bridge_Tracks {
 		add_filter( 'jetpack_woocommerce_analytics_event_props', array( $this, 'filter_jetpack_woocommerce_analytics_event_props' ) );
 		add_filter( 'woocommerce_admin_survey_query', array( $this, 'set_survey_source' ) );
 
+		// Hide WooCommerce.com advanced settings page.
+		add_filter( 'woocommerce_com_integration_settings', array( $this, 'hide_woocommerce_tracking_setting' ), 10, 1 );
+
 		// Always opt-in to Tracks, WPCOM user tracks preferences take priority.
 		add_filter( 'woocommerce_apply_tracking', '__return_true' );
 		add_filter( 'woocommerce_apply_user_tracking', '__return_true' );
@@ -204,9 +207,16 @@ class WC_Calypso_Bridge_Tracks {
 	 *
 	 * @param array $settings Current settings array.
 	 */
-	public function hide_woocommerce_com_settings( $settings ) {
-		unset( $settings['woocommerce_com'] );
-		return $settings;
+	public function hide_woocommerce_tracking_setting( $settings ) {
+		return array_filter(
+			$settings,
+			function ( $setting ) {
+				if ( empty( $setting['id'] ) ) {
+					return $setting;
+				}
+				return ! in_array( $setting['id'], [ 'tracking_options', 'woocommerce_allow_tracking' ] );
+			}
+		);
 	}
 
 	/**
